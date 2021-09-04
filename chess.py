ec1b9e528f291db5ea3566a228c747405efd17a3
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2021-09-04 11:59:14 +0600

    Uploading Chess

---
+++ chess.py
@@ -0,0 +1,21 @@
+print("Welcome To Chess")
+
+lenghtTwo =[]
+
+lenghtThree =[]
+
+lenghtFour = []
+
+moves = ['e4','e5','Qf3','Nc6','Bc4','a6','Qb7#']
+
+for i in moves:
+    if len(i) >= 2 and len(i) <= 4:
+        pass
+    else:
+        print("Move is out of range")
+
+    
+
+
+	
+	
